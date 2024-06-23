b1eabe9f1bfe190821a4c9ba6e4a48d882d9f928
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2024-06-23 19:08:04 -0300

    [django-cadastro] - finalizando projeto

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,8 @@ from cadastro import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
 
 
--- app/cadastro/views.py
+++ app/cadastro/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 def home(request):
     return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    # Salvando os dados da tela para o banco de dados
+    novo_usuario = Usuarios()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    # Exibir todos os usuários Já cadastrados em uma nova página 
+    usuarios = {
+        'usuarios': Usuarios.objects.all()
+    }
+
+    # Retornar os dados para a página de listagem de usuário
+    return render(request, 'usuarios/usuarios.html', usuarios)
